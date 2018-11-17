5b1960580aec992dab9e7a5ecb600903aa4a5e59
Hakim Ghods <ghods.hakim@hotmail.com>
2018-11-17 17:44:18 +0330

    Add project files.

---
+++ BindingDemo/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BindingDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index");
+            return Ok();
+        }
+    }
+}
